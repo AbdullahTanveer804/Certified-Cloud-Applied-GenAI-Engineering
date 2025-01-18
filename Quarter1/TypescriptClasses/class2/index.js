c110c743cbdc40de16d4edfe9f47d5d2e599066d
AbdullahTanveer804 <abdullahdev804@gmail.com>
2025-01-19 00:33:21 +0500

    Learn Operators

---
+++ Quarter1/TypescriptClasses/class2/index.js
@@ -0,0 +1,39 @@
+"use strict";
+console.log('Hello, world!');
+// Operators
+// Arithmetic operators
+// +, -, *, /, %, ++, --
+// Assignment operators
+// =, +=, -=, *=, /=, %=
+// Comparison operators
+// ==, !=, >, <, >=, <=
+// Logical operators
+// &&, ||, !
+// Bitwise operators
+// &, |, ^, ~, <<, >>
+// Type operators
+// instanceof, typeof
+// String operators
+// +, +=, ``
+// Conditional (ternary) operator
+// ?:
+// Comma operator
+// ,
+// Unary operators
+// delete, typeof, void
+// Relational operators
+// in, instanceof
+// Equality operators
+// ==, ===, !=, !==
+// Bitwise shift operators
+// <<, >>, >>>
+// Spread operator  
+// ...
+// Destructuring operator
+// { }
+// Optional chaining operator
+// ?.
+// Nullish coalescing operator
+// ??
+// Logical nullish assignment operator
+// ??=
