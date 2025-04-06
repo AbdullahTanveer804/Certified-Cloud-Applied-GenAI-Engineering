ac88e3c09c3f033439c2395dcb045a1996acf12f
AbdullahTanveer804 <abdullahdev804@gmail.com>
2025-04-06 19:59:10 +0500

    Basic Cli Number Guessing Game

---
+++ Quarter1/NODE-Projects/01_Number_Guessing_Game/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+// Title of Program
+async function sleep() {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 2000); // 2 seconds
+    });
+}
+async function welcome() {
+    const title = chalkAnimation.rainbow("Welcom to Number Guessing Game!"); // Animation Starting
+    await sleep();
+    title.stop(); // Animation Stopping
+}
+await welcome();
+// Main Function
+// Random Number Generation
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "numberguessed",
+        type: "number",
+        message: chalk.bgWhite("Guess a number between 1 & 10 :")
+    }
+]);
+const { numberguessed } = answer; // Destructuring the answer object
+if (numberguessed === randomNumber) {
+    console.log(chalk.green("Congratulaton You Guessed The Right Number!"));
+}
+else {
+    console.log(chalk.red("You Guessed The Wrong Number!"));
+    console.log(chalk.yellow(`The Correct Number Was ${randomNumber}`));
+}
